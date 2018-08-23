da7a83c065adc84e37efa4f25c3ba07e03711e6d
duji <jid@hwtc.com.cn>
2018-08-23 14:59:54 +0800

    init project

---
+++ app/src/main/java/cn/com/hwtc/aidlclient/MainActivity.java
@@ -0,0 +1,114 @@
+package cn.com.hwtc.aidlclient;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.List;
+
+import cn.com.hwtc.aidlserver.CalculateInterface;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "AidlClient " + MainActivity.class.getSimpleName();
+    EditText etNumOne;
+    EditText etNumTwo;
+    TextView calculateResult;
+    CalculateInterface calculateInterface;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tvBindRemoteService = findViewById(R.id.tv_bind_remote_service);
+        etNumOne = findViewById(R.id.et_num_one);
+        etNumTwo = findViewById(R.id.et_num_two);
+        calculateResult = findViewById(R.id.calculate_result);
+        TextView tvCalculateAdd = findViewById(R.id.tv_calculate_add);
+        TextView tvCalculateSubtract = findViewById(R.id.tv_calculate_subtract);
+        TextView tvCalculateMultiply = findViewById(R.id.tv_calculate_multiply);
+        tvBindRemoteService.setOnClickListener(this);
+        tvCalculateAdd.setOnClickListener(this);
+        tvCalculateSubtract.setOnClickListener(this);
+        tvCalculateMultiply.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_bind_remote_service:
+                Intent intent = createExplicitIntent(new Intent("cn.com.hwtc.aidlserver.CalculateService"));
+                if (intent != null) {
+                    boolean bindService = bindService(intent, mConnection, BIND_AUTO_CREATE);
+                    Log.d(TAG, "bindService:" + bindService);
+                }
+                break;
+            case R.id.tv_calculate_add:
+                assert calculateInterface != null;
+                try {
+                    int i = calculateInterface.doCalculateAdd(Integer.parseInt(etNumOne.getText().toString()), Integer.parseInt(etNumTwo.getText().toString()));
+                    calculateResult.setText(String.valueOf("计算结果:" + i));
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.tv_calculate_subtract:
+                assert calculateInterface != null;
+                try {
+                    int i = calculateInterface.doCalculateSubtract(Integer.parseInt(etNumOne.getText().toString()), Integer.parseInt(etNumTwo.getText().toString()));
+                    calculateResult.setText(String.valueOf("计算结果:" + i));
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.tv_calculate_multiply:
+                assert calculateInterface != null;
+                try {
+                    int i = calculateInterface.doCalculateMultiply(Integer.parseInt(etNumOne.getText().toString()), Integer.parseInt(etNumTwo.getText().toString()));
+                    calculateResult.setText(String.valueOf("计算结果:" + i));
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private Intent createExplicitIntent(Intent intent) {
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> resolveInfo = pm.queryIntentServices(intent, 0);
+        if (resolveInfo != null && resolveInfo.size() == 1) {
+            ResolveInfo serviceInfo = resolveInfo.get(0);
+            String packageName = serviceInfo.serviceInfo.packageName;
+            String className = serviceInfo.serviceInfo.name;
+            ComponentName component = new ComponentName(packageName, className);
+            Intent explicitIntent = new Intent(intent);
+            explicitIntent.setComponent(component);
+            return explicitIntent;
+        } else {
+            return null;
+        }
+    }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            calculateInterface = CalculateInterface.Stub.asInterface(iBinder);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            calculateInterface = null;
+        }
+    };
+}
